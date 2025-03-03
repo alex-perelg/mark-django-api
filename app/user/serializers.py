31a4a004cdd5c175785361f28c5290afcc9411f1
alex-perelg <aperelg@gmail.com>
2025-03-03 11:44:50 +0700

    Add up to 'Implement recipe listing API'

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -19,6 +19,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
@@ -36,7 +47,7 @@ class AuthTokenSerializer(serializers.Serializer):
             request=self.context.get("request"), username=email, password=password
         )
         if not user:
-            msg = "Unable to authenticate with provided credentials."
+            msg = _("Unable to authenticate with provided credentials.")
             raise serializers.ValidationError(msg, code="authentication")
 
         attrs["user"] = user
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -131,7 +131,7 @@ class PrivateUserApiTests(TestCase):
 
         res = self.client.patch(ME_URL, payload)
 
-        self.user.resfesshfrom_db()
+        self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload["name"])
         self.assertTrue(self.user.check_password(payload["password"]))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
